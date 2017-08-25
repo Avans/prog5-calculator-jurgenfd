2310ada5eb92b46049439fa4e45473b753ff5fa8
jurgenfd <jurgenfd@gmail.com>
2017-08-25 18:28:07 +0200

    Test the world.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator
-{
-        class Program
-    {
+namespace Calculator {
+    class Program {
         static void Main(string[] args) {
-            Console.WriteLine(    "Success!");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
